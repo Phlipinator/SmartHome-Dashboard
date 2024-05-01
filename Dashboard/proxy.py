3f80b89e8208c6cd6ff806379a111a2a8c0d47e1
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2024-05-01 16:03:58 +0200

    debugging connection

--- Dashboard/proxy.py
+++ Dashboard/proxy.py
@@ -14,8 +14,8 @@ class Proxy:
         self.config = Config()
 
     def read_proxy_data(self):
-        # if not self.is_plugged_in:
-        #     return None
+        if not self.is_plugged_in:
+            return None
         try:
             # Read data from the proxy at the specified device address
             data = self.bus.read_i2c_block_data(self.address, 0x00, 8)
@@ -27,3 +27,47 @@ class Proxy:
         except IOError as e:
             print(f"Read Error: {e}")
             return None
+
+    def calculate_voltage(self, raw_value, type):
+        adc = (raw_value / 4095.0) * 3.3
+        if type == "tile":
+            return adc * 1.68
+        elif type == "row":
+            return adc * 1.51
+        elif type == "col":
+            return adc * 1.51  # Example if same factor as row
+        return adc
+
+    def convert_value(self, raw_value, type):
+
+        voltage = self.calculate_voltage(raw_value, type)
+        data_list = getattr(self.config, f"{type}List")
+        threshold = self.config.thresholds[type]
+
+        for voltage_level, number in data_list:
+            if voltage_level - threshold <= voltage <= voltage_level + threshold:
+                return number
+        return 0  # If no range matches
+
+    def get_position(self):
+        # First check connection to the device
+        self.check_connection()
+        data = self.read_proxy_data()
+
+        if data is None:
+            print("No data received from the device or device not connected.")
+            return None  # Exit the function if no data is received
+
+        tile_value, row_value, col_value = data  # Now it's safe to unpack
+
+        tile = self.convert_value(tile_value, "tile")
+        row = self.convert_value(row_value, "row")
+        col = self.convert_value(col_value, "col")
+
+        # Retrieve and apply row and column adjustments based on the tile number
+        if tile > 0:  # Assuming tile numbering starts at 1
+            row_adjustment, col_adjustment = self.config.adjustmentTable[tile - 1]
+            adjusted_row = row + row_adjustment
+            adjusted_col = col + col_adjustment
+            return adjusted_row, adjusted_col
+        return None
--- Dashboard/test.py
+++ Dashboard/test.py
@@ -6,6 +6,6 @@ from proxy import Proxy
 # Create an instance of the smbus library
 bus = smbus.SMBus(1)  # 1 indicates /dev/i2c-1
 
-proxy = Proxy(0x36, bus)  # Device I2C address
+proxy = Proxy(bus, 0x36)  # Device I2C address
 position = proxy.get_position()
 print(position)
