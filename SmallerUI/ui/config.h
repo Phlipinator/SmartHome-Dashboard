4f89400509e2924e557c42636daad644a8235f02
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2024-05-14 12:44:00 +0200

    New Backup of Proxy Arduino Project

---
+++ SmallerUI/ui/config.h
@@ -0,0 +1,31 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// CHANGE PROXY ID HERE
+const int ID = 2;
+const String topic = "Proxy" + String(ID);
+
+// WIFI Credentials
+const char* ssid = "";
+const char* password = "";
+
+// MQTT Broker settings
+const char* mqtt_server = "";
+const int mqtt_port = 1883;
+const char* mqtt_user = "";
+const char* mqtt_password = "";
+
+// Initialize Position Pins
+const byte TILE_PIN = 25;
+const byte ROW_PIN = 32;
+const byte COL_PIN = 33;
+
+// Initialize Encoder Pins
+const int encoderPinA = 27;
+const int encoderPinB = 14;
+
+// Screen resolution
+static const uint16_t screenWidth = 240;
+static const uint16_t screenHeight = 240;
+
+#endif // CONFIG_H
