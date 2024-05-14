5f6f18372ee0590129b7a1ba98341314fef221ef
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2024-05-14 13:57:33 +0200

    Removes IP-Print from MQTT broker

--- MQTT Control/MQTT-Broker.ino
+++ MQTT Control/MQTT-Broker.ino
@@ -13,9 +13,10 @@ void setup() {
     WiFi.softAP("Dashboard", "muchPrivate");
 
     // Print the IP address of the ESP32
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
+    // IPAddress IP = WiFi.softAPIP();
+    // Serial.print("AP IP address: ");
+    // Serial.println(IP);
+    // IP: 192.168.4.1
 
     // Subscribe to a topic pattern and attach a callback
     mqtt.subscribe("#", [](const char* topic, const char* payload) {
