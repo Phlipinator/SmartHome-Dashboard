9de739e2a81cf15d856d3a12afb141361f9ff55c
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2024-05-18 19:38:06 +0200

    Implement queue functionality into lightController

--- Dashboard/lightController.py
+++ Dashboard/lightController.py
@@ -1,4 +1,6 @@
+import threading
 import time
+from queue import Queue
 
 import serial
 
@@ -6,11 +8,26 @@ import serial
 class LightController:
     def __init__(self, port, baudrate):
         self.serial_port = serial.Serial(port, baudrate, timeout=1)
-    
+        self.message_queue = Queue()
+        self.worker_thread = threading.Thread(target=self._send_messages)
+        self.worker_thread.daemon = True  # Daemonize the thread so it will automatically stop when the main program exits
+        self.worker_thread.start()
+
+    def _send_messages(self):
+        while True:
+            message = self.message_queue.get()
+            self.serial_port.write(message.encode())
+            time.sleep(5)  # Minimum delay of 5 seconds between messages
+
     def send_coordinates(self, x, y):
         message = f"{x},{y}\n"
-        self.serial_port.write(message.encode())
-    
+        self.message_queue.put(message)
+
     def send_path(self, x1, y1, x2, y2):
         message = f"{x1},{y1},{x2},{y2}\n"
-        self.serial_port.write(message.encode())
+        self.message_queue.put(message)
+
+# Example usage
+light_controller = LightController("/dev/ttyUSB0", 9600)
+light_controller.send_coordinates(10, 20)
+light_controller.send_path(30, 40, 50, 60)
