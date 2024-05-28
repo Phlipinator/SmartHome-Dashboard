b48d492fb94c07244c8dd9fe4f1e5d7167b5df4b
Philipp Thalhammer <thalhammerphilipp@gmail.com>
2024-05-28 15:11:30 +0200

    Include separate log dir

--- Dashboard/logger.py
+++ Dashboard/logger.py
@@ -1,15 +1,23 @@
 import datetime
 import logging
+import os
 from logging.handlers import TimedRotatingFileHandler
 
 
 class Logger:
     def __init__(self, log_file=None):
+        # Ensure the logs directory exists
+        log_dir = 'logs'
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
         if log_file is None:
             log_file = datetime.datetime.now().strftime('%Y-%m-%d') + ".log"
+        
+        log_file_path = os.path.join(log_dir, log_file)
         self.logger = logging.getLogger("MyLogger")
         self.logger.setLevel(logging.INFO)
-        handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1)
+        handler = TimedRotatingFileHandler(log_file_path, when="midnight", interval=1)
         handler.suffix = "%Y-%m-%d"
         handler.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
